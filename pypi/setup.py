778a0ead33ba0505a62fb1c060e5a01d6f517ad8
HSJoss <162680708+HSJoss@users.noreply.github.com>
2024-04-13 23:11:58 +0900

    Add files via upload

---
+++ pypi/setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+    name ="HSJworld",
+    version="0.0.1",
+    description="HSJ's library",
+    author="HSJ",
+    url="https://github.com/HSJoss/my-oss",
+    download_url="https://github.com/HSJoss/my-oss/",
+    packages=["mylib"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
