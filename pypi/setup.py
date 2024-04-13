78d23d3fae022ede99d4ab1e3d02c52a68adb241
HSJoss <162680708+HSJoss@users.noreply.github.com>
2024-04-13 22:36:57 +0900

    Add files via upload

--- pypi/setup.py
+++ pypi/setup.py
@@ -2,13 +2,13 @@
 import setuptools
 
 setuptools.setup(
-    name ="HSJCalc",
+    name ="HSJLib",
     version="0.0.1",
-    description="HSJ's calculator",
+    description="HSJ's library",
     author="HSJ",
-    url="https://github.com/HSJOss/calc0001",
-    download_url="https://github.com/HSJOss/calc0001/",
-    packages=["mycalc"],
+    url="https://github.com/HSJoss/my-oss",
+    download_url="https://github.com/HSJoss/my-oss/",
+    packages=["mylib"],
     classifiers=[
         "Programming Language :: Python :: 3",
     ]
