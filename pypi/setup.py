c11ce2bbff6f8a3b92f79f9fa361c6a7abc9ff9a
HSJoss <162680708+HSJoss@users.noreply.github.com>
2024-04-13 20:21:51 +0900

    Add files via upload

---
+++ pypi/setup.py
@@ -0,0 +1,15 @@
+
+import setuptools
+
+setuptools.setup(
+    name ="HSJCalc",
+    version="0.0.1",
+    description="HSJ's calculator",
+    author="HSJ",
+    url="https://github.com/HSJOss/calc0001",
+    download_url="https://github.com/HSJOss/calc0001/",
+    packages=["mycalc"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ]
+)
